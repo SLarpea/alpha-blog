c9b841a407b9bd81520bac1f6c3e89c45ad0cad8
asulana <arnold.sulana@gmail.com>
2019-08-07 13:56:36 +0800

    add secure password and attribute to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 
 class User < ApplicationRecord
+
   has_many :articles
   before_save { self.email = email.downcase }
   validates :username, presence: true,
@@ -9,5 +10,5 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: 105},
             uniqueness: {case_sensitive: false},
             format: {with: VALID_EMAIL_REGEX }
-
+  has_secure_password
 end
