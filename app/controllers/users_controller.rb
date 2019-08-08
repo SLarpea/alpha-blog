144b815f8145760253e049e32a6ded447d8a944e
asulana <arnold.sulana@gmail.com>
2019-08-08 09:54:28 +0800

    Fixed #3 DRY

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def new
     @user = User.new
   end
@@ -19,15 +21,14 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account is successfully updated"
       redirect_to articles_path
@@ -37,6 +38,11 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
