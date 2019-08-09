a531d54ec2c2e1078623369efd51aa3fa8f91c97
asulana <arnold.sulana@gmail.com>
2019-08-09 18:41:46 +0800

    adjust set category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,7 @@
 class CategoriesController < ApplicationController
 
   before_action :require_admin, except: [:index, :show]
+  before_action :set_category, only: [:edit, :update, :show]
 
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
@@ -22,11 +23,11 @@ class CategoriesController < ApplicationController
   end
 
   def edit
-    @category = Category.find(params[:id])
+
   end
 
   def update
-    @category = Category.find(params[:id])
+
     if @category.update(category_params)
       flash[:success] = "Category name was successfully updated"
       redirect_to category_path(@category)
@@ -36,11 +37,15 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @category = Category.find(params[:id])
+
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
   private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
   def category_params
     params.require(:category).permit(:name)
